760dffa92501303e65dab568a9f345b5aa102185
koreanthuglife <rlghks358@naver.com>
2023-03-07 19:37:10 +0900

    add optimizing scheduling widget

--- optimizing_schedule/lib/screen/available_schedule.dart
+++ optimizing_schedule/lib/screen/available_schedule.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+class Optimizing_Schedult extends StatefulWidget {
+  const Optimizing_Schedult({super.key});
+
+  @override
+  State<Optimizing_Schedult> createState() => _Optimizing_SchedultState();
+}
+
+class _Optimizing_SchedultState extends State<Optimizing_Schedult> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+          child: Column(
+        children: [
+          Row(),
+          ListView(),
+        ],
+      )),
+    );
+  }
+}
