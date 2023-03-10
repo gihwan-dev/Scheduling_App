5c7139460f4f1d0d776218242d58c9573a728e45
koreanthuglife <rlghks358@naver.com>
2023-03-10 10:07:07 +0900

    something

--- optimizing_schedule/lib/component/adding_subjects.dart
+++ optimizing_schedule/lib/component/adding_subjects.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 const List<String> day = ['월', '화', '수', '목', '금'];
 const List<String> time = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 
+class Subject {
+  String title = '';
+  String score = '';
+  List time = [];
+}
+
 class AddingSubjects extends StatefulWidget {
-  const AddingSubjects({super.key});
+  const AddingSubjects({
+    super.key,
+  });
 
   @override
   State<AddingSubjects> createState() => _AddingSubjectsState();
@@ -12,17 +20,24 @@ class AddingSubjects extends StatefulWidget {
 
 class _AddingSubjectsState extends State<AddingSubjects> {
   late TextEditingController textEditingController;
+  late TextEditingController scoreController;
   String title = '';
   String dropdownDay = day.first;
+  String dropdowntime = time.first;
+  String score = '';
+  List curTime = [];
+  List<Subject> subjectList = [];
 
   @override
   void initState() {
-    textEditingController = TextEditingController();
+    textEditingController = new TextEditingController();
+    scoreController = new TextEditingController();
   }
 
   @override
   void dispose() {
     textEditingController.dispose();
+    scoreController.dispose();
     super.dispose();
   }
 
@@ -51,12 +66,71 @@ class _AddingSubjectsState extends State<AddingSubjects> {
                 ),
               ),
             ),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 dayButton(),
+                timeButton(),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        curTime.add([dropdownDay, dropdowntime]);
+                      });
+                    },
+                    child: Text('추가')),
               ],
-            )
+            ),
+            Container(
+              width: 50,
+              child: TextField(
+                controller: scoreController,
+                decoration: InputDecoration(
+                  label: Text('학점'),
+                ),
+                onChanged: (value) => {score = value},
+                onSubmitted: (value) => {
+                  setState(() {
+                    score = value;
+                  })
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 200,
+              height: 150,
+              child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: List.generate(curTime.length, (index) {
+                    return ListTile(
+                      title: Text('${curTime[index][0]}'),
+                      subtitle: Text('${curTime[index][1]}'),
+                      onLongPress: () => {
+                        setState(() {
+                          curTime.removeAt(index);
+                        }),
+                      },
+                    );
+                  })),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  Subject temp = new Subject();
+                  temp.title = title;
+                  temp.score = score;
+                  temp.time.add(curTime);
+                  subjectList.add(temp);
+                  curTime = [];
+                });
+              },
+              child: Text('저장'),
+            ),
           ],
         ),
       ),
@@ -77,5 +151,22 @@ class _AddingSubjectsState extends State<AddingSubjects> {
             dropdownDay = value;
           });
         });
+  }
+
+  DropdownButton timeButton() {
+    return DropdownButton(
+      value: dropdowntime,
+      items: time.map((value) {
+        return DropdownMenuItem(
+          child: Text(value),
+          value: value,
+        );
+      }).toList(),
+      onChanged: (value) {
+        setState(() {
+          dropdowntime = value;
+        });
+      },
+    );
   }
 }
--- optimizing_schedule/lib/screen/available_schedule.dart
+++ optimizing_schedule/lib/screen/available_schedule.dart
@@ -12,65 +12,76 @@ class Optimizing_Schedult extends StatefulWidget {
 }
 
 class _Optimizing_SchedultState extends State<Optimizing_Schedult> {
+  bool saveChecked = false;
+
+  void savedCheckedfuntion() {
+    setState(() {
+      saveChecked = !saveChecked;
+    });
+  }
+
   int checkState = 1;
   @override
   Widget build(BuildContext context) {
     return Dialog(
       child: Container(
+          width: 300,
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(10)),
-              child: TextButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) => AddingSubjects());
-                },
-                child: Text(checkState == 1
-                    ? '필수과목 추가하기'
-                    : (checkState == 2 ? '듣고싶은 과목 추가하기' : '들을 수 있는 과목 추가하기')),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AddingSubjects());
+                    },
+                    child: Text(checkState == 1
+                        ? '필수과목 추가하기'
+                        : (checkState == 2
+                            ? '듣고싶은 과목 추가하기'
+                            : '들을 수 있는 과목 추가하기')),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 400,
-              child: ListView(),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: TextButton(
-                onPressed: () {
-                  if (checkState == 1) {
-                    setState(() {
-                      checkState = 2;
-                    });
-                  } else if (checkState == 2) {
-                    setState(() {
-                      checkState = 3;
-                    });
-                  } else {
-                    setState(() {
-                      Navigator.pop(context);
-                      checkState = 1;
-                    });
-                  }
-                },
-                child: Text(checkState == 3 ? '완료' : '다음'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 400,
+                  child: ListView(),
+                ),
               ),
-            ),
-          ),
-        ],
-      )),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: TextButton(
+                    onPressed: () {
+                      if (checkState == 1) {
+                        setState(() {
+                          checkState = 2;
+                        });
+                      } else if (checkState == 2) {
+                        setState(() {
+                          checkState = 3;
+                        });
+                      } else {
+                        setState(() {
+                          Navigator.pop(context);
+                          checkState = 1;
+                        });
+                      }
+                    },
+                    child: Text(checkState == 3 ? '완료' : '다음'),
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
